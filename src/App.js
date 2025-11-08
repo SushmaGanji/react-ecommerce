c8abe7b5598432d530d41c55223313624f9ad7e0
sushma <sushma.ganji002@gmail.com>
2025-11-08 21:08:06 +0530

    Checkout page in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import HomePage from './components/pages/HomePage.jsx'
+import CheckOutPage from './components/pages/CheckOutPage.jsx'
+import {Routes,Route} from 'react-router'
 function App() {
   return (
     <div  className='App'>
-<HomePage/>
+      <Routes>
+        <Route path="/" element={ <HomePage/>}></Route>
+        <Route path="checkout" element={ <CheckOutPage/>}></Route>
+     
+      </Routes>
     </div>
   );
 }
